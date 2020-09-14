5db60a1f8a965a08504604eaed4c630c2dcb2309
John Robling <roblingjohn@gmail.com>
2020-09-14 16:45:59 -0400

    added database name

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = `mongodb+srv://${username}:${password}@cluster0.pde5d.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+const db = `mongodb+srv://${username}:${password}@cluster0.pde5d.mongodb.net/onward-theatre?retryWrites=true&w=majority`;
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
