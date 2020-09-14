9e4cd3c4dcf7a31f6294c4782f1fb48167f9d05b
John Robling <roblingjohn@gmail.com>
2020-09-14 14:28:37 -0400

    set up app

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  // useEffect(() => {
+  //   axios.get("/api/config").then(res => {
+  //     console.log(res.data)
+  //   });
+  // });
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/config", (req, res) => {
+  res.json({ success: true });
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// app.use(routes);
+
+app.get("*"),
+  (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  };
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/onward-theatre", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to database.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to database.");
+    console.log(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Epxress server running on http://localhost:${PORT}`);
+});
