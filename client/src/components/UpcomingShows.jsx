6055e8b88fd42433ff7a83a2ba0a33baa7c58ce0
John Robling <roblingjohn@gmail.com>
2020-09-15 14:01:47 -0400

    separated show info into separate component and added conditional rendering based on date

--- client/src/components/UpcomingShows.jsx
+++ client/src/components/UpcomingShows.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ShowInfo from "./ShowInfo";
 
 class UpcomingShows extends Component {
   state = {
@@ -24,22 +25,47 @@ class UpcomingShows extends Component {
         description:
           "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod doloribus fuga libero voluptatum modi nisi tempore eaque sed repellendus voluptatibus, similique inventore recusandae. Magnam optio eaque quo dolores ab ipsam.",
       },
+      {
+        showName: "Improvised Drinking Game",
+        showTime: { showDate: "Saturday", showTime: "11:00" },
+        tagline: "Totally not a ripoff of Improv Asshole",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim unde aliquam culpa ullam autem incidunt minima dolore amet a veritatis, tenetur recusandae harum impedit, esse, molestias quae. Sapiente, adipisci fugit?",
+      },
     ],
   };
   render() {
     return (
       <div>
-        <h1>Upcoming Shows:</h1>
+        <h1>This Weekend at Onward:</h1>
         <h2>Friday</h2>
-        {this.state.shows.map((show) => (
-          <>
-            <h3>
-              {show.showTime.showTime} {show.showName}
-            </h3>
-            <h4>{show.tagline}</h4>
-            <p>{show.description}</p>
-          </>
-        ))}
+        {this.state.shows.map(
+          (show) =>
+            show.showTime.showDate === "Friday" && (
+              <>
+                <ShowInfo
+                  showTime={show.showTime.showTime}
+                  showName={show.showName}
+                  tagline={show.tagline}
+                  description={show.description}
+                />
+              </>
+            )
+        )}
+        <h2>Saturday</h2>
+        {this.state.shows.map(
+          (show) =>
+            show.showTime.showDate === "Saturday" && (
+              <>
+                <ShowInfo
+                  showTime={show.showTime.showTime}
+                  showName={show.showName}
+                  tagline={show.tagline}
+                  description={show.description}
+                />
+              </>
+            )
+        )}
       </div>
     );
   }
