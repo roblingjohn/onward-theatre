0f405607fb5205ab1cb22cdc7a3ba6d1c31ca83a
John Robling <roblingjohn@gmail.com>
2020-09-15 13:17:38 -0400

    created jumbotron

---
+++ client/src/components/OTJumbotron.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const OTJumbotron = () => {
+  return (
+    <div className="jumbotron">
+      <h1>Welcome to Onward Theatre</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+        vero. Debitis, totam. Hic sit sint, quis incidunt, repudiandae
+        voluptatibus adipisci recusandae neque fuga molestias omnis beatae ut
+        expedita voluptatem eum?
+      </p>
+    </div>
+  );
+};
+
+export default OTJumbotron;
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import OTJumbotron from '../components/OTJumbotron';
 
 const Home = () => {
     return (
         <div>
+            <OTJumbotron/>
             <h1>Hello world</h1>
         </div>
     );
