aeef411cd1c4b585ce283c38306beeef69d2c154
John Robling <roblingjohn@gmail.com>
2020-09-15 15:00:37 -0400

    added better dummy data

--- client/src/containers/Classes.jsx
+++ client/src/containers/Classes.jsx
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Classes extends Component {
-    render() {
-        return (
-            <div>
-                <h1>This is the classes page</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Classes</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae rem
+          libero incidunt, delectus facilis distinctio amet ullam ut assumenda
+          nihil quo animi porro perspiciatis adipisci qui eaque nemo! Inventore,
+          animi?
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quaerat
+          quasi temporibus maxime sunt a deserunt eos ipsa itaque, dicta,
+          voluptate ut molestiae possimus mollitia dolorum ea, adipisci error
+          explicabo?
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
+          possimus, maiores, sequi earum repellendus nam, sunt sint excepturi
+          iure voluptatibus perferendis. A quasi officia sapiente fugiat ex?
+          Omnis, rem quos?
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Classes;
--- client/src/containers/Contact.jsx
+++ client/src/containers/Contact.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div>
-            <h1>This is the contact page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Contact</h1>
+      <h3>123 Fake Street Atlanta, GA 30300</h3>
+      <h3>
+        Email:{" "}
+        <a href="mailto:onwardtheatre@gmail.com">onwardtheatre@gmail.com</a>
+      </h3>
+    </div>
+  );
 };
 
 export default Contact;
