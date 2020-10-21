872e478af2b75beb9a6e544f9e2b34404a7d2a2f
John Robling <roblingjohn@gmail.com>
2020-10-21 18:31:57 -0400

    added map showing atlanta to contact page

--- client/src/containers/Contact.jsx
+++ client/src/containers/Contact.jsx
@@ -6,6 +6,11 @@ const Contact = () => {
       <h1>Contact</h1>
       <h3>123 Fake Street Atlanta, GA 30300</h3>
       {/* When the theater location is decided on, this will include a Google Maps box showing it */}
+      <iframe
+        src="https://www.google.com/maps/d/embed?mid=1qfR92O-H17kOQC0qBgOT3HE7UdY"
+        width="640"
+        height="480"
+      ></iframe>
       <h3>
         Email:{" "}
         <a href="mailto:onwardtheatre@gmail.com">onwardtheatre@gmail.com</a>
